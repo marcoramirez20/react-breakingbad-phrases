c60195edfbcb10e47d864b687f6a2992ff13daec
Marco Ramirez <marcoramirez20@gmail.com>
2021-10-22 22:48:46 -0300

    Se suben los archivos del proyecto.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Este proyecto se encuentra publicado en: https://objective-clarke-6c20d6.netlify.app/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{  useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Phrase from './components/Phrase';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574D 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #FFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  :hover { 
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+
+  const [phrase, setPhrase] = useState({});
+
+  const getPhrase = async (e) => {
+    const phraseFromAPI = await fetch('https://breakingbadapi.com/api/quote/random').then(response => {
+      return response.json();
+    });
+    setPhrase(phraseFromAPI[0]);
+  }
+
+  useEffect(() => {
+    getPhrase();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Phrase
+        phrase={phrase}
+      />
+      <Button
+        onClick={getPhrase}
+      >
+        Obtener Frase
+      </Button>
+    </Container>
   );
 }
 
